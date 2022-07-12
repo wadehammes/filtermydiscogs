5de43b07440f94c39101c1299372fddfa84090ac
Wade Hammes <wade.hammes@gmail.com>
2022-07-12 11:45:12 -0400

    fix linting

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,10 +1,9 @@
-import { FontWeight } from "src/interfaces/common.interfaces";
 import { device } from "src/styles/theme";
 import styled from "styled-components";
 
 export const H1 = styled.h1`
   font-size: 1.5rem;
-  font-weight: ${FontWeight.Bold};
+  font-weight: bold;
   padding-bottom: 1.5rem;
   line-height: 1.1;
 
@@ -14,7 +13,7 @@ export const H1 = styled.h1`
 `;
 
 export const P = styled.p`
-  font-weight: ${FontWeight.Regular};
+  font-weight: normal;
   line-height: 1.5;
   padding-bottom: 1.5rem;
   font-size: 1rem;
