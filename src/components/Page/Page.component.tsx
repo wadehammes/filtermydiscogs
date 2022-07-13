e50638171b9ff153654f9b3070deb814e568d229
Wade Hammes <wade.hammes@gmail.com>
2022-07-13 09:12:19 -0400

    add gradient

--- src/components/Page/Page.component.tsx
+++ src/components/Page/Page.component.tsx
@@ -6,7 +6,13 @@ import styled from "styled-components";
 const PageBackground = styled.div`
   position: fixed;
   inset: 0;
-  background-image: linear-gradient(315deg, #fee2f8 0%, #dcf8ef 74%);
+  background: linear-gradient(238.72deg, #ebff00 0%, #8f00ff 100%),
+    linear-gradient(64.82deg, #ad00ff 0%, #ff0000 100%),
+    linear-gradient(65.03deg, #00ffff 0%, #ff0000 99.79%),
+    radial-gradient(67.08% 100% at 50% 100%, #ff00c7 0%, #50005e 100%),
+    radial-gradient(100% 140% at 100% 0%, #5ed500 0%, #2200aa 100%);
+  background-blend-mode: color-dodge, difference, lighten, color-dodge, normal;
+  opacity: 0.35;
   z-index: -1;
   height: 100vh;
   width: 100vw;
