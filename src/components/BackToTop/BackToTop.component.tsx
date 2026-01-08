b07b891b2b9e0d5d63bb9cd915f33bc9a009014e
Wade Hammes <wade.hammes@gmail.com>
2026-01-07 19:59:35 -0500

    Refactor and cleanup CSS; use custom-media

--- src/components/BackToTop/BackToTop.component.tsx
+++ src/components/BackToTop/BackToTop.component.tsx
@@ -38,8 +38,6 @@ export const BackToTop = () => {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
