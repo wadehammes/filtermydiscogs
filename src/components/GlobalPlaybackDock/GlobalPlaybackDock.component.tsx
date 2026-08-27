0bed184535bfc7d82f1ca40499b437dc8150140b
Wade Hammes <wadehammes@users.noreply.github.com>
2026-08-27 11:53:54 -0400

    PlayerBar z-index update (#902)

--- src/components/GlobalPlaybackDock/GlobalPlaybackDock.component.tsx
+++ src/components/GlobalPlaybackDock/GlobalPlaybackDock.component.tsx
@@ -1,22 +1,30 @@
 "use client";
 
 import classNames from "classnames";
+import { createPortal } from "react-dom";
 import { ReleaseMiniPlayer } from "src/components/ReleaseMiniPlayer/ReleaseMiniPlayer.component";
 import { usePlaybackReleaseClickHandler } from "src/context/playbackReleaseClick.context";
 import { useIsMiniPlayerVisible } from "src/context/releasePlayback.context";
+import { useMounted } from "src/hooks/useMounted.hook";
 import playbackDockStyles from "src/styles/modules/playback-dock.module.css";
 import styles from "./GlobalPlaybackDock.module.css";
 
 export const GlobalPlaybackDock = () => {
+  const mounted = useMounted();
   const isMiniPlayerVisible = useIsMiniPlayerVisible();
   const onReleaseClick = usePlaybackReleaseClickHandler();
 
-  return (
+  if (!mounted) {
+    return null;
+  }
+
+  return createPortal(
     <div
       className={classNames(styles.dock, playbackDockStyles.dockGlobals)}
       {...(isMiniPlayerVisible ? { "data-global-playback-dock": true } : {})}
     >
       <ReleaseMiniPlayer onReleaseClick={onReleaseClick} />
-    </div>
+    </div>,
+    document.body,
   );
 };
